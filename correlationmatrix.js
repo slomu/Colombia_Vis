10eeadcad3cf7aabd15ead9f2c58d464b6f6ae93
Nathanael Zahn <nz25dyzu@studserv.uni-leipzig.de>
2023-06-20 13:42:14 +0200

    fixed scaling issues updated tooltips updated visuals to look nicer

--- correlationmatrix.js
+++ correlationmatrix.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 const margin = {top: 80, right: 25, bottom: 30, left: 100},
-  width = 1000 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  width = 1250 - margin.left - margin.right,
+  height = 750 - margin.top - margin.bottom;
 
 function arrayMin(arr) {
     var len = arr.length, min = Infinity;
@@ -29,9 +29,14 @@ function format_dataset(d, indicators){
     //for(let i in d){
     for(let i = 0; i < d.length; i++){
         for(let k in indicators){
-            let ind = indicators[k]
-            let row = {dep: d[i].dep, variable: ind, value: d[i][ind]};
-            //console.log(row)
+            let row = {};
+            let ind = indicators[k];
+            if(ind == "gini"){
+                row = {dep: d[i].dep, variable: ind, value: parseFloat((d[i][ind] * 100))};
+            }else{
+                row = {dep: d[i].dep, variable: ind, value: parseFloat(d[i][ind])};
+            }
+            console.log(row)
             dict[count] = row;
             count += 1;
         }
@@ -51,28 +56,9 @@ var svg = d3.select("#my_dataviz")
 //Read the data
 d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/data.csv").then(function(data) {
     // Labels of row and columns N,Nhh,urban,iwi,iwipov70,iwipov50,iwipov35,gini,qhousing,edyr20,womedyr20,menedyr20,edchild,workwom,agedifmar,infmort
-    //const myGroups = Array.from(new Set(data.map(d => d.dep))) //Array.from(new Set(data.map(d => d.group)))
-    //const myVars = Array.from(new Set(data.map(d => d.cases))) //Array.from(new Set(data.map(d => d.variable)))
-    const myValues = Array.from(new Set(data.map(d => d.population))) //Array.from(new Set(data.map(d => d.value)))
-
     const departements = {"SANTAFE DE BOGOTA D.C": "DC", "AMAZONAS": "AM", "ANTIOQUIA": "AN", "ARAUCA": "AR", "ATLANTICO": "AT", "BOLIVAR": "BL", "BOYACA": "BY", "CALDAS": "CL", "CAQUETA": "CQ", "CASANARE": "CS", "CAUCA": "CA", "CESAR": "CE", "CHOCO": "CH", "CORDOBA": "CO", "CUNDINAMARCA": "CU", "GUAINIA": "GN", "GUAVIARE": "GV",  "HUILA": "HU", "GUAJIRA": "LG", "MAGDALENA": "MA", "META": "ME", "NARINO": "NA", "NORTE DE SANTANDER": "NS", "PUTUMAYO": "PU", "QUINDIO": "QD", "RISARALDA": "RI", "SAN ANDRES": "SA", "SANTANDER": "ST", "SUCRE": "SU", "TOLIMA": "TO", "VALLE": "VC", "VAUPES": "VP", "VICHADA": "VD"}
     //var dep_abb = Object.values(deps)
     const indicators = ["urban","iwi","iwipov70","iwipov50","iwipov35","gini","qhousing","edyr20","womedyr20","menedyr20","edchild","workwom","agedifmar","infmort"]
-    //const departements = Array.from(new Set(data.map(d => d.dep)))
-    //const urban = Array.from(new Set(data.map(d => d.urban)))
-    //const iwi = Array.from(new Set(data.map(d => d.iwi)))
-    //const iwipov70 = Array.from(new Set(data.map(d => d.iwipov70)))
-    //const iwipov50 = Array.from(new Set(data.map(d => d.iwipov50)))
-    //const iwipov35 = Array.from(new Set(data.map(d => d.iwipov35)))
-    //const gini = Array.from(new Set(data.map(d => d.gini)))
-    //const qhousing = Array.from(new Set(data.map(d => d.qhousing)))
-    //const edyr20 = Array.from(new Set(data.map(d => d.edyr20)))
-    //const womedyr20 = Array.from(new Set(data.map(d => d.womedyr20)))
-    //const menedyr20 = Array.from(new Set(data.map(d => d.menedyr20)))
-    //const edchild = Array.from(new Set(data.map(d => d.edchild)))
-    //const workwom = Array.from(new Set(data.map(d => d.workwom)))
-    //const agedifmar = Array.from(new Set(data.map(d => d.agedifmar)))
-    //const infmort = Array.from(new Set(data.map(d => d.infmort)))
 
     var dataset = format_dataset(data, indicators);
 
@@ -93,18 +79,12 @@ d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/d
     //    console.log([min, max])
     //    return [min, max]
     //}
-    
-    var radius = d3.scaleSqrt()
-        .range([3, 20])
-        //.domain([arrayMin(dataset.value), arrayMax(dataset.value)])
-        .domain([0,100])
-        .clamp(true);
 
     // Build X scales and axis:
     const x = d3.scaleBand()
-        .range([ 0, width ])
+        .range([0, width])
         .domain(Object.values(departements))
-        .padding(0.05);
+        .padding(0.0);
     svg.append("g")
         .style("font-size", 15)
         //.attr("transform", `translate(0, ${height})`)
@@ -113,13 +93,19 @@ d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/d
 
     // Build Y scales and axis:
     const y = d3.scaleBand()
-        .range([ height, 0 ])
+        .range([height, 0])
         .domain(indicators)
         .padding(0.05);
     svg.append("g")
         .style("font-size", 15)
         .call(d3.axisLeft(y).tickSize(0))
         .select(".domain").remove()
+
+    var radius = d3.scaleLinear()
+        .range([5, x.bandwidth()/2])
+        //.domain([arrayMin(dataset.value), arrayMax(dataset.value)])
+        .domain([0,100])
+        .clamp(true);
 
     // Build color scale
     color = d3.scaleOrdinal(d3.schemeCategory10)
@@ -142,12 +128,15 @@ d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/d
         tooltip
             .style("opacity", 1)
         d3.select(this)
-            .style("stroke", "black")
-            .style("opacity", 1)
+            .style("stroke", "blue")
+            .style("fill-opacity", .8)
     }
     const mousemove = function(event,d) {
         tooltip
-            .html("The exact value of<br>this cell is: " + d.population)//d.value)
+            .html("Departement: " + d.dep 
+                + "<br>Indicator: " + d.variable 
+                + "<br>Value: " + Math.round((d.value + Number.EPSILON) * 100) / 100
+            )
             .style("left", (event.x)+20 + "px")
             .style("top", (event.y) + "px")
      }
@@ -155,8 +144,8 @@ d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/d
         tooltip
             .style("opacity", 0)
         d3.select(this)
-        .style("stroke", "none")
-        .style("opacity", 1)
+        .style("stroke", function(d) { return color(d.variable)})
+        .style("fill-opacity", 0.1)
     }
     //console.log(Object.keys(data))
 
@@ -171,9 +160,11 @@ d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/d
         })
         //.attr("cy", function(d) { return y(indicators)+radius(Infinity)})//.variable)+radius(Infinity)})
         //.attr("r", function(d) {  return radius(d.population)} )//d.value)} )
-        .style("fill", function(d) { return color(d.variable)} )//d.value)} ) 
+        .style("fill", function(d) { return color(d.variable)} )
+        .style("fill-opacity", 0.1)
+        .style("stroke", function(d) { return color(d.variable)} )
         .style("stroke-width", 4)
-        .style("stroke", "none")
+        //.style("stroke", "none")
         .on("mouseover", mouseover)
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
