371bbfe004b464a75c9c1ec5ecace8a4c192d007
Nathanael Zahn <nz25dyzu@studserv.uni-leipzig.de>
2023-06-25 02:05:28 +0200

    finished sorting interactions

--- correlationmatrix.js
+++ correlationmatrix.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 const margin = {top: 20, right: 25, bottom: 30, left: 100}, 
-    width = 1250 - margin.left - margin.right,
-    height = 750 - margin.top - margin.bottom;
+    width = 1400 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
 
 // set initial components
 const dep_init = {"SANTAFE DE BOGOTA D.C": "DC", "AMAZONAS": "AM", "ANTIOQUIA": "AN", "ARAUCA": "AR", "ATLANTICO": "AT", "BOLIVAR": "BL", "BOYACA": "BY", "CALDAS": "CL", "CAQUETA": "CQ", "CASANARE": "CS", "CAUCA": "CA", "CESAR": "CE", "CHOCO": "CH", "CORDOBA": "CO", "CUNDINAMARCA": "CU", "GUAINIA": "GN", "GUAVIARE": "GV",  "HUILA": "HU", "GUAJIRA": "LG", "MAGDALENA": "MA", "META": "ME", "NARINO": "NA", "NORTE DE SANTANDER": "NS", "PUTUMAYO": "PU", "QUINDIO": "QD", "RISARALDA": "RI", "SAN ANDRES": "SA", "SANTANDER": "ST", "SUCRE": "SU", "TOLIMA": "TO", "VALLE": "VC", "VAUPES": "VP", "VICHADA": "VD"}
@@ -75,23 +75,51 @@ get_data = function(indicators, departements){
     d3.csv("https://media.githubusercontent.com/media/slomu/Colombia_Vis/main/data/data.csv").then(function(data) {
 
         // update Datapoints
-        const click_ind = function(event, d, dataset){
+        const click_ind = function(event, d){
+            dep_ordered = {};
+            dataset = format_dataset(data, indicators, departements);
             filtered = dataset.filter(obj => {
                 return obj.variable === d
             })
             ordered = filtered.sort(function(a, b){return b.value - a.value});
-            console.log(ordered);
+            for(sample of ordered){
+                dep_ordered[sample.dep] = dep_init[sample.dep];
+            }
+            departements = dep_ordered;
+
+            return(draw(data, indicators, departements))
+        }
+
+        const click_dep = function(event){
+            dep_ordered = {};
+            dataset = format_dataset(data, indicators, departements);
+            filtered = dataset.filter(obj => {
+                return Object.keys(dep_init).includes(obj.dep)
+            })
+            filtered.sort( function (a, b) {
+                var A = a["dep"], B = b["dep"];
+                
+                if (Object.keys(dep_init).indexOf(A) > Object.keys(dep_init).indexOf(B)) {
+                  return 1;
+                } else {
+                  return -1;
+                } 
+            });
+
+            for(sample of filtered){
+                dep_ordered[sample.dep] = dep_init[sample.dep];
+            }
+            departements = dep_ordered;
+            return draw(data, indicators, departements)
         }
 
         // create Data
         function format_dataset(d, indicators, departements){
             let dict = [];
             let count = 0;
-            console.log(d)
             filtered = d.filter(obj => {
                 return Object.keys(departements).includes(obj.dep)
             })
-            console.log(filtered)
             for(let i = 0; i < filtered.length; i++){
                 for(ind of indicators){
                     let row = {};
@@ -159,7 +187,6 @@ get_data = function(indicators, departements){
 
         // Three function that change the tooltip when user hover / move / leave a cell
         const mouseover_data = function(event,d) {
-            console.log("Hello")
             tooltip_data
                 .style("opacity", 1)
             d3.select(this)
@@ -198,7 +225,6 @@ get_data = function(indicators, departements){
                     .attr("transform", `translate(${margin.left},${margin.top})`);
 
             var dataset = format_dataset(data, indicators, departements);
-            console.log(dataset);
 
             // Build Y scales and axis:
             const y = d3.scaleBand()
@@ -206,10 +232,11 @@ get_data = function(indicators, departements){
                 .domain(indicators)
                 .padding(0.05);
             svg.append("g")
+                .attr("id", "yaxis")
                 .style("font-size", 15)
                 .call(d3.axisLeft(y).tickSize(0))
                 .select(".domain").remove()
-            svg.selectAll('g.tick')
+            svg.selectAll('#yaxis .tick')
                     .on("mouseover", mouseover_ind)
                     .on("mousemove", mousemove_ind)
                     .on("mouseleave", mouseleave_ind)   
@@ -219,11 +246,14 @@ get_data = function(indicators, departements){
             const x = d3.scaleBand()
                 .range([0, width])
                 .domain(Object.values(departements))
-                .padding(0.05);
+                .padding(0.05)
             svg.append("g")
+                .attr("id", "xaxis")
                 .style("font-size", 15)
                 .call(d3.axisTop(x).tickSize(0))
                 .select(".domain").remove()
+            svg.selectAll('#xaxis').on("click", click_dep)
+            
 
             // Scale for bubble sizes
             var radius = d3.scaleLinear()
@@ -244,7 +274,7 @@ get_data = function(indicators, departements){
                 .style("fill", function(d) { return color(d.variable)} )
                 .style("fill-opacity", 0.1)
                 .style("stroke", function(d) { return color(d.variable)} )
-                .style("stroke-width", 4)
+                .style("stroke-width", 3)
                 .on("mouseover", mouseover_data)
                 .on("mousemove", mousemove_data)
                 .on("mouseleave", mouseleave_data);
