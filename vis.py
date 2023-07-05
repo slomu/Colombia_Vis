cee67c028c59367591090e2f1e0d5171437a4e7a
slomu <jl60zopa@studserv.uni-leipzig.de>
2023-07-05 16:15:24 +0200

    fixes

--- vis.py
+++ vis.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import json
+import numpy as np
+
+def convert_int64(item):
+    if isinstance(item, np.int64):
+        return int(item)
+    return item
 
 app = Flask(__name__)
 
@@ -7,7 +14,9 @@ app = Flask(__name__)
 def my_form():
     df = pd.read_csv('data/dv_data.csv')
     df2 = pd.read_csv("data/popdata.csv")
-    cities = pd.read_csv("data/counts.csv")
+    #with open('data/counts.json', 'r') as openfile:
+    #    cities = json.load(openfile)
+        
     df3 = df["DEPARTAMENTO"].value_counts().to_dict().items()
     col = "1"
     dft = {}
@@ -33,15 +42,47 @@ def my_form():
     dfw = {}
     for u,v in df6:
         dfw[u] = int(v)
-    
-    return render_template("choropleth.html", data = dft, dataG = dfg, dataA = dfa, dataW = dfw, col = col, dataC = cities)
+        
+               
+    citydata = pd.read_csv("data/worldcities.csv")
+    #df = pd.read_csv("data/dv_data.csv")
+    counts = df["MUNICIPIO"].value_counts()
+    #counts.to_csv("data/counts.csv")
+
+    result = {}
+    for city in citydata["city_ascii"]:
+        try:
+            dep = str(citydata[citydata["city_ascii"] == city].admin_name).split()[1]
+            if dep == "NORTE":
+                dep = "NORTE DE SANTANDER"
+            if dep == "SANTAFE":
+                dep = "SANTAFE DE BOGOTA D.C"
+
+            if dep not in result:
+                result[dep] = []
+            
+            result[dep].append({
+                "city": city,
+                "long": float(citydata[citydata["city_ascii"] == city].lng),
+                "lat": float(citydata[citydata["city_ascii"] == city].lat),
+                "cases": counts[city]
+            })
+        except (KeyError, TypeError):
+            pass    
+        
+    u_data = json.dumps(result, default=convert_int64)
+    city_data = json.loads(u_data)
+        
+    return render_template("choropleth.html", data = dft, dataG = dfg, dataA = dfa, dataW = dfw, col = col, dataC = city_data)
 
 @app.route("/", methods = ["POST"])
 def index():
     if request.form["action"] == "Submit":
         df = pd.read_csv('data/dv_data.csv')
         df2 = pd.read_csv("data/popdata.csv")
-        cities = pd.read_csv("data/counts.csv")
+        #with open('data/counts.json', 'r') as openfile:
+        #    cities = json.load(openfile)
+        
         c1 = request.form["weapon"]
         c2 = request.form["age"]
         c3 = request.form["gender"]
@@ -79,8 +120,39 @@ def index():
         dfw = {}
         for u,v in df6:
             dfw[u] = int(v)
+            
+        citydata = pd.read_csv("data/worldcities.csv")
+        #df = pd.read_csv("data/dv_data.csv")
+        counts = df["MUNICIPIO"].value_counts()
+        #counts.to_csv("data/counts.csv")
 
-        return render_template("choropleth.html" , data = dft, dataG = dfg, dataA = dfa, dataW = dfw, col = col, dataC = cities, selected_weapon = c1, selected_age = c2, selected_gender = c3)
+        result = {}
+        for city in citydata["city_ascii"]:
+            try:
+                dep = str(citydata[citydata["city_ascii"] == city].admin_name).split()[1]
+                if dep == "NORTE":
+                    dep = "NORTE DE SANTANDER"
+                if dep == "SANTAFE":
+                    dep = "SANTAFE DE BOGOTA D.C"
+
+
+                if dep not in result:
+                    result[dep] = []
+                
+                result[dep].append({
+                    "city": city,
+                    "long": float(citydata[citydata["city_ascii"] == city].lng),
+                    "lat": float(citydata[citydata["city_ascii"] == city].lat),
+                    "cases": counts[city]
+                })
+            except (KeyError, TypeError):
+                pass
+            
+        u_data = json.dumps(result, default=convert_int64)
+        city_data = json.loads(u_data)
+
+
+        return render_template("choropleth.html" , data = dft, dataG = dfg, dataA = dfa, dataW = dfw, col = col, selected_weapon = c1, selected_age = c2, selected_gender = c3, dataC = city_data)
 
 
 if __name__ == "__main__":
